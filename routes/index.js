c9d7f8a36609b29a2153f13e3094aa1eb4edd7c9
sachmundassery <69303859+sachmundassery@users.noreply.github.com>
2020-11-18 22:13:58 +0530

    signup page & db connected

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.post('/submit',function(req,res){
+  console.log(req.body)
+MongoClient.connect('mongodb://localhost:27017',function(err,client){
+  if(err){
+     console.log("Error")
+  }
+  else{
+    console.log("DB Connected")
+    client.db('sampleDB').collection('user').insertOne(req.body) // we have created a db and added a collection
+  }
+})
+  res.send('gotcha')
+})
+
 
 module.exports = router;
